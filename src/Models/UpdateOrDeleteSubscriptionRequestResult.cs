9973a4baecbc2f204378ed8a2a9bbd17983f3e3e
Ercenk Keresteci <ercenk@microsoft.com>
2019-06-10 15:06:31 -0700

    Implemented Retry-After header

---
+++ src/Models/UpdateOrDeleteSubscriptionRequestResult.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+
+namespace SaaSFulfillmentSDKUnofficial.Client.Models
+{
+    public class UpdateOrDeleteSubscriptionRequestResult : FullfilmentRequestResult
+    {
+        private const string operationLocationKey = "Operation-Location";
+        private const string retryAfterKey = "Retry-After";
+
+        public Uri Operation { get; set; }
+
+        public int RetryAfter { get; set; }
+
+        protected override void UpdateFromHeaders(HttpHeaders headers)
+        {
+            base.UpdateFromHeaders(headers);
+
+            IEnumerable<string> values;
+            if (headers.TryGetValues(operationLocationKey, out values))
+            {
+                Uri operationUri;
+                Uri.TryCreate(values.First(), UriKind.Absolute, out operationUri);
+
+                if (operationUri == null)
+                {
+                    throw new ApplicationException("API did not return an operation ID");
+                }
+                this.Operation = operationUri;
+            }
+
+            values = default(IEnumerable<string>);
+
+            if (headers.TryGetValues(retryAfterKey, out values))
+            {
+                int retryAfter = 0;
+
+                int.TryParse(values.First(), out retryAfter);
+
+                if (retry == null)
+                {
+                    throw new ApplicationException("API did not return a retry-after value");
+                }
+                this.Operation = operationUri;
+            }
+        }
+    }
+}
